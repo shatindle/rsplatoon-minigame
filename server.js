a3697ce38be07ee4caaf41b1ddac3ea8cff7b55e
Shane Tindle <sha.tindle@gmail.com>
2021-01-02 14:32:41 -0600

    Added SSL support

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
+const appSettings = require("./settings.json");
 
 const app = express();
 
-var server = require("https").createServer(app);
+const key = appSettings.secure ? fs.readFileSync('./key.pem') : null;
+const cert = appSettings.secure ? fs.readFileSync('./cert.pem') : null;
+
+var server = appSettings.secure ? 
+    require("https").createServer({key: key, cert: cert }, app) : 
+    require("http").createServer(app);
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 
 require("./pages/game")(app);
 
-server.listen(443);
+if (appSettings.secure) {
+    server.listen(443);
+} else {
+    server.listen(80);
+}
